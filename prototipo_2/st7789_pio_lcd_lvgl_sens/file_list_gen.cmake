f8f618e4c9d88c1d6228d1fb4b3a7839862b9456
Paulo Santos <132781052+paulosnf12@users.noreply.github.com>
2025-11-02 17:16:58 -0300

    Delete prototipo_2 directory

--- prototipo_2/st7789_pio_lcd_lvgl_sens/file_list_gen.cmake
+++
@@ -1,9 +0,0 @@
-list(
-  APPEND
-  PROJECT_SOURCES
-  ${CMAKE_CURRENT_LIST_DIR}/components/buttons/button_normal_gen.c
-  ${CMAKE_CURRENT_LIST_DIR}/components/buttons/button_show_numpad_gen.c
-  ${CMAKE_CURRENT_LIST_DIR}/components/list/list_gen.c
-  ${CMAKE_CURRENT_LIST_DIR}/screens/screen_animations_gen.c
-  ${CMAKE_CURRENT_LIST_DIR}/ui_animations_gen.c
-  ${CMAKE_CURRENT_LIST_DIR}/ui_animations.c)
--- prototipo_2/temp-hum-dew-screen/src/ui/images.c
+++
@@ -1,8 +0,0 @@
-#include "images.h"
-
-const ext_img_desc_t images[4] = {
-    { "humidity", &img_humidity },
-    { "temperature", &img_temperature },
-    { "hwit", &img_hwit },
-    { "dew", &img_dew },
-};
